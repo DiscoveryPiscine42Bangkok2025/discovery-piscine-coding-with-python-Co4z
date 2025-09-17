e8db6b805930a172cead43ca909b616fb931b3ed
Co4z <93990701+Co4z@users.noreply.github.com>
2025-09-17 21:40:14 +0700

    Update play_with_arrays.py

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,10 +1,5 @@
-# play_with_arrays.py
-
 arr = [2, 8, 9, 48, 8, 22, -12, 2]
-
-# filter เอาค่าที่มากกว่า 5
 filtered = [x for x in arr if x > 5]
-
-# แสดงผล
 print(arr)
 print([x + 2 for x in filtered])
+
