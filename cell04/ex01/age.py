5d5890bb63197cde0d1957a20b495e68d4e4788a
Co4z <93990701+Co4z@users.noreply.github.com>
2025-09-17 21:46:19 +0700

    Update age.py

--- cell04/ex01/age.py
+++ cell04/ex01/age.py
@@ -1,2 +1,4 @@
-my_age = 22
-print(my_age+42)
+age = int(input("input your age : "))
+print('You are currently',age,'years old.')
+for i in range(10,40,10):
+    print("In",i,"years, you'll be",age+i,"years old.")
